79e81e34ad0c199a439ae2fdea91e197d6f2772d
Juan Antonio Brena Moral <Juan.Brena.Moral@ing.com>
2018-09-28 14:05:21 +0200

    Adding Contracts for a second API

---
+++ META-INF/com.example/beer-api-producer-git/0.0.1-SNAPSHOT/contracts/api2/rest/shouldReturnEvenWhenRequestParamIsEven.groovy
@@ -0,0 +1,17 @@
+
+org.springframework.cloud.contract.spec.Contract.make {
+    ignored()
+    description "should return even when number input is even"
+    request{
+        method GET()
+        url("/validate/prime-number") {
+            queryParameters {
+                parameter("number", "2")
+            }
+        }
+    }
+    response {
+        body("Even")
+        status 200
+    }
+}
